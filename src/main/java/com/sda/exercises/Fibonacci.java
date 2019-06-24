6594511d793b2a12cbbfd2f8f2314f58b35bba13
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-06-24 19:46:15 +0200

    utworzenie statycznej metody arrayCreate

--- src/main/java/com/sda/exercises/Fibonacci.java
+++ src/main/java/com/sda/exercises/Fibonacci.java
@@ -1,4 +1,21 @@
 package com.sda.exercises;
 
 public class Fibonacci {
+
+    public static Long[]arrayCreate(int n){
+        Long []array=new Long[n];
+        int length=array.length;
+
+        if (length>0){
+            array[0]=0L;
+        }
+        if (length>1){
+            array[1]=1L;
+        }
+
+        for (int i=2;i<length;i++){
+            array[i]=array[i-2]+array[i-1];
+        }
+        return array;
+    }
 }
