3616d14b89c01e228e8bbed02a7f446d6c0def54
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-06-24 20:02:59 +0200

    edycja klasy Main

---
+++ src/main/java/com/sda/exercises/Main.java
@@ -0,0 +1,14 @@
+package com.sda.exercises;
+
+
+public class Main {
+    public static void main(String[] args) {
+     Long[]longs= Fibonacci.arrayCreate(12);
+
+     for (Long i:longs){
+         System.out.println(i+" ");
+     }
+
+
+    }
+}
